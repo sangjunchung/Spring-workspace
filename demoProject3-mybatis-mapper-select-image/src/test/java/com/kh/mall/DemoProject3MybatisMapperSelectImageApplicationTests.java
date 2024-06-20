09c510e75083802571add7a6bb3e41a62ae62b45
sangjunchung <wjtkwn19@gmail.com>
2024-06-20 13:09:58 +0900

    select join image

---
+++ demoProject3-mybatis-mapper-select-image/src/test/java/com/kh/mall/DemoProject3MybatisMapperSelectImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProject3MybatisMapperSelectImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
