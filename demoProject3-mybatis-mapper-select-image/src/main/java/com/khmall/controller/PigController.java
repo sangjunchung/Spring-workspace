bfbe26406df1c4da96e51088ba1934d6a34ebf5c
sangjunchung <wjtkwn19@gmail.com>
2024-06-20 14:38:39 +0900

    image

--- demoProject3-mybatis-mapper-select-image/src/main/java/com/khmall/controller/PigController.java
+++ demoProject3-mybatis-mapper-select-image/src/main/java/com/khmall/controller/PigController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.khmall.dto.Pig;
 import com.khmall.service.PigService;
@@ -24,11 +25,18 @@ public class PigController {
 		return "index";
 	}
 	
-	@GetMapping("/pig/{pig_id}")
+	@GetMapping("/pigDetail/{pig_id}")
 	public String getPigById(Model model, @PathVariable int pig_id) {
 		Pig pig = pigService.getPigById(pig_id);
 		model.addAttribute("pig", pig);
 		
 		return "pigDetail";
 	}
+	/*
+	// -> application.properties에 error 경로를 지정하고
+	//    설정에서 지정된 경로를 찾아 발생
+	@RequestMapping("/error")
+	public String getError() {
+		return "error";
+	}*/
 }
